31147744b0f4b9f89168cc7ff24bb676f21889ba
Omkar138 <OKulkarni14@gmail.com>
2021-08-12 02:41:14 +0530

    first commit

---
+++ dist/index.66f7bf0b.js
@@ -0,0 +1,2 @@
+const btn=document.querySelector(".btn"),body=document.querySelector(".body");let a="a variable";btn.addEventListener("click",(function(){console.log("javascript code is running"),btn.textContent="JS code is running",body.style.backgroundColor="red"})),console.log(a);
+//# sourceMappingURL=index.66f7bf0b.js.map
---
+++ main.js
@@ -0,0 +1,12 @@
+const btn=document.querySelector(".btn");
+const body=document.querySelector(".body");
+let a= "a variable";
+
+
+btn.addEventListener("click",function()
+{
+    console.log("javascript code is running");
+    btn.textContent="JS code is running";
+    body.style.backgroundColor="red";
+})
+console.log(a);
